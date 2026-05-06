a9f1f2fa2fe900715b19c290618d38c5b1f6a049
cpricamata <czairra.paulette.ricamata@student.ateneo.edu>
2026-05-06 19:40:02 +0800

    Update models.py

--- shemu/shemuapp/models.py
+++ shemu/shemuapp/models.py
@@ -24,7 +24,6 @@ class Employee(models.Model):
         else:
             return self.overtime_pay
     
-    
     def resetOvertime(self):
         self.overtime_pay = 0
 
@@ -37,3 +36,66 @@ class Employee(models.Model):
         
     def __str__(self):
         return f"{self.pk}, rate:{self.rate}"
+
+    class Payslip(models.Model): #Payslip Attributes
+        id_number = models.ForeignKey('Employee', on_delete=models.CASCADE, related_name='payslips') 
+        #on_delete=models.CASCADE : https://www.freecodecamp.org/news/how-to-use-a-foreign-key-in-django/
+        month = models.CharField(max_length=20)
+        date_range = models.CharField(max_length=50)
+        year = models.CharField(max_length=4)
+        pay_cycle = models.IntegerField()
+        rate = models.FloatField()
+        earnings_allowance = models.FloatField(default=0)
+        deductions_tax = models.FloatField(default=0)
+        deductions_health = models.FloatField(default=0)
+        pag_ibig = models.FloatField(default=0)
+        sss = models.FloatField(default=0)
+        overtime = models.FloatField(default=0)
+        total_pay = models.FloatField()
+
+    def getIDNumber(self): #Methods
+        return self.id_number
+
+    def getMonth(self):
+        return self.month
+
+    def getDate_range(self):
+        return self.date_range
+
+    def getYear(self):
+        return self.year
+
+    def getPay_cycle(self):
+        return self.pay_cycle
+
+    def getRate(self):
+        return self.rate
+
+    def getCycleRate(self):
+        return self.rate / 2
+
+    def getEarnings_allowance(self):
+        return self.earnings_allowance
+
+    def getDeductions_tax(self):
+        return self.deductions_tax
+
+    def getDeductions_health(self):
+        return self.deductions_health
+
+    def getPag_ibig(self):
+        return self.pag_ibig
+
+    def getSSS(self):
+        return self.sss
+
+    def getOvertime(self):
+        return self.overtime
+
+    def getTotal_pay(self):
+        return self.total_pay
+
+    def __str__(self):
+        return "pk: {}, Employee: {}, Period: {} {}, {}, Cycle: {}, Total Pay: {}".format(self.pk, self.id_number, self.month, self.date_range, self.year, self.pay_cycle, self.total_pay)
+
+
