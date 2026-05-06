c4d4f70d5b4c267d0293083ab5bd33492f459030
carmenfid <carmenfrancescaduran@gmail.com>
2026-05-07 03:54:09 +0800

    views, template, urls

---
+++ shemu/shemuapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.employees, name='employees'),
+    path('create-employee/', views.create_employee, name='create_employee'),
+    path('update-employee/', views.update_employee, name='update_employee'),
+    path('delete-employee/', views.delete_employee, name='delete_employee'),
+    path('add-overtime/', views.add_overtime, name='add_overtime'),
+    path('payslips/', views.payslips, name='payslips'),
+    path('view-payslip/<int:payslip_id>/', views.vew_payslip, name='view_payslip'),
+]
--- shemu/shemuapp/views.py
+++ shemu/shemuapp/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee, Payslip
 
 # Create your views here.
+
+def employees(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees.html', {'employees': employees})
+
+def create_employee(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        id_number = request.POST['id_number']
+        rate = float(request.POST['rate'])
+        allowance = request.POST.get('allowance', 0)
+        if allowance == '':
+            allowance = 0
+        
+        Employee.objects.create(
+            name=name,
+            id_number=id_number,
+            rate=rate,
+            allowance=float(allowance)
+        )
+        return redirect('employees')
+    return render(request, 'create_employee.html')
+
+def update_employee(request, id_number):
+    if request.method == 'POST':
+        if 'id_number' in request.POST and 'name' not in request.POST:
+            id_number = request.POST.get('id_number')
+            employee = get_object_or_404(Employee, id_number=id_number)
+            return render(request, 'update_employee.html', {'employee': employee})
+        Employee.objects.filter(if_number=id_number).update(
+            name=request.POST.get('name'),
+            id_number=request.POST.get('rate'),
+            rate=float(request.POST.get('rate')),
+            allowance=float(request.POST.get('allowance', 0) or 0)
+        )
+        return redirect ('employees')
+    return redirect('employees')
+
+def delete_employee(request, id_number):
+    if request.method == 'POST':
+        id_number = request.POST.get('id_number')
+        Employee.objects.filter(id_number=id_number).delete()
+    return redirect('employees')
+
+def add_overtime(request, id_number):
+    if request.method == 'POST':
+        id_number = request.POST.get('id_number')
+        employee = get_object_or_404(Employee, id_number=id_number)
+        hours = float(request.POST.get('overtime_hours', 0))
+
+        overtime_amount = (employee.rate /160) * 1.5 * hours
+
+        if employee.overtime_pay:
+            new_overtime = employee.overtime_pay + overtime_amount
+        else:
+            new_overtime = overtime_amount
+
+        Employee.objects.filter(id_number=id_number).update(
+            overtime_pay=new_overtime
+        )
+    return redirect('employees')
+
+def payslips(request):
+    all_employees = Employee.objects.all()
+    all_payslips = Payslip.objects.all()
+
+    #will cont this since i can't see Payslips model yet ;;
