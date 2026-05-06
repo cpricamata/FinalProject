77b6422f77a52ab54e08e1054612a1e1524fcf47
carmenfid <carmenfrancescaduran@gmail.com>
2026-05-07 04:24:51 +0800

    fixed indentation in models

--- shemu/shemuapp/models.py
+++ shemu/shemuapp/models.py
@@ -37,21 +37,21 @@ class Employee(models.Model):
     def __str__(self):
         return f"{self.pk}, rate:{self.rate}"
 
-    class Payslip(models.Model): #Payslip Attributes
-        id_number = models.ForeignKey('Employee', on_delete=models.CASCADE, related_name='payslips') 
-        #on_delete=models.CASCADE : https://www.freecodecamp.org/news/how-to-use-a-foreign-key-in-django/
-        month = models.CharField(max_length=20)
-        date_range = models.CharField(max_length=50)
-        year = models.CharField(max_length=4)
-        pay_cycle = models.IntegerField()
-        rate = models.FloatField()
-        earnings_allowance = models.FloatField(default=0)
-        deductions_tax = models.FloatField(default=0)
-        deductions_health = models.FloatField(default=0)
-        pag_ibig = models.FloatField(default=0)
-        sss = models.FloatField(default=0)
-        overtime = models.FloatField(default=0)
-        total_pay = models.FloatField()
+class Payslip(models.Model): #Payslip Attributes
+    id_number = models.ForeignKey('Employee', on_delete=models.CASCADE, related_name='payslips') 
+    #on_delete=models.CASCADE : https://www.freecodecamp.org/news/how-to-use-a-foreign-key-in-django/
+    month = models.CharField(max_length=20)
+    date_range = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)
+    pay_cycle = models.IntegerField()
+    rate = models.FloatField()
+    earnings_allowance = models.FloatField(default=0)
+    deductions_tax = models.FloatField(default=0)
+    deductions_health = models.FloatField(default=0)
+    pag_ibig = models.FloatField(default=0)
+    sss = models.FloatField(default=0)
+    overtime = models.FloatField(default=0)
+    total_pay = models.FloatField()
 
     def getIDNumber(self): #Methods
         return self.id_number
